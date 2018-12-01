86318f509d6c4097206809937e26cc20cc104171
Juri <juri.dispan@me.com>
2018-12-01 15:04:19 +0100

    Update day01.hs

--- day01.hs
+++ day01.hs
@@ -1,4 +1,5 @@
 -- Day 1
+-- Assumes that the input file is saved as "inp1a.txt". 
 
 -- a)
 import Data.List.Split (splitOn)
@@ -8,7 +9,8 @@ findSeqSum = scanl (\acc x -> acc + readToInt x) 0
     where
         readToInt str | head str == '+' = (read $ tail str)
                       | otherwise       = -(read $ tail str)
-        
+
+-- First part
 ch1 = do
     let file = "inp1a.txt"
     contents <- readFile file
@@ -19,7 +21,8 @@ dup xs = dup' xs Set.empty
   where dup' [] _     = error "Never happens ;)"
         dup' (x:xs) s | Set.member x s = x
                       | otherwise      = dup' xs (Set.insert x s)
-                       
+
+-- Second part
 ch2 = do
     let file = "inp1a.txt"
     contents <- readFile file
