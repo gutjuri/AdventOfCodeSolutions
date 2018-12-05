c39af5d5b8548a79df069949400a477743cf54e0
Juri <juri.dispan@me.com>
2018-12-05 10:09:29 +0100

    Update day05.hs

--- day05.hs
+++ day05.hs
@@ -3,10 +3,11 @@
 
 import Data.Char(isLower, toLower)
 
+-- Part 1
+
 xor a b = (a && not b) || (not a && b)
 
-cancelsOut x1 x2 = toLower x1 == toLower x2
-                      && xor (isLower x1) (isLower x2) 
+cancelsOut x1 x2 = toLower x1 == toLower x2 && xor (isLower x1) (isLower x2) 
 
 sweepOnce (x1:x2:xs) | cancelsOut x1 x2 = sweepOnce xs
                      | otherwise        = x1:sweepOnce (x2:xs)
@@ -22,11 +23,11 @@ ch1 = do
     contents <- readFile "inp5a.txt"
     return $ length $ untilStable sweepOnce contents
     
-    
--- Solution 2
+-- Part 2
 
 chars = "abcdefghijklmnopqrstuvwxyz"
 
+-- Solution 2
 ch2 = do
     contents <- readFile "inp5a.txt"
     let red = untilStable sweepOnce contents
