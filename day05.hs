98ea62c7da28197defd95e2d14f72c2ac42a972c
Juri <juri.dispan@me.com>
2018-12-05 10:07:39 +0100

    Create day05.hs

---
+++ day05.hs
@@ -0,0 +1,33 @@
+-- Day 5
+-- assumes the input to be stored in inp5a.txt
+
+import Data.Char(isLower, toLower)
+
+xor a b = (a && not b) || (not a && b)
+
+cancelsOut x1 x2 = toLower x1 == toLower x2
+                      && xor (isLower x1) (isLower x2) 
+
+sweepOnce (x1:x2:xs) | cancelsOut x1 x2 = sweepOnce xs
+                     | otherwise        = x1:sweepOnce (x2:xs)
+sweepOnce r = r
+
+untilStable f xs | xs == p = p
+                 | otherwise = untilStable f p
+    where
+        p = f xs
+
+-- Solution 1        
+ch1 = do
+    contents <- readFile "inp5a.txt"
+    return $ length $ untilStable sweepOnce contents
+    
+    
+-- Solution 2
+
+chars = "abcdefghijklmnopqrstuvwxyz"
+
+ch2 = do
+    contents <- readFile "inp5a.txt"
+    let red = untilStable sweepOnce contents
+    return $ minimum $ map length $ map (untilStable sweepOnce) $ map (\chr -> filter (\x -> chr /= toLower x) red) chars
