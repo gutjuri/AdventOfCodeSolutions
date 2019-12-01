e0cd78dac6cf0650cc22ebb524cb5c905c89ef6c
Juri Dispan <juri.dispan@uni-ulm.de>
2019-12-01 13:05:06 +0100

    solved day1 2019

---
+++ 2019/day01.hs
@@ -0,0 +1,20 @@
+module Main where
+
+import Control.Monad
+
+main :: IO ()
+main = do
+  inp <- map read . lines <$> readFile "inputs/input01.txt"
+  let sol1 = ch1 inp
+      sol2 = ch2 inp
+  print sol1
+  print sol2
+
+ch1 :: [Int] -> Int
+ch1 = sum . map ((subtract 2) .  (`div` 3))
+
+ch2 :: [Int] -> Int
+ch2 = sum . map totalFuel
+  where 
+    f = (subtract 2). (`div` 3)
+    totalFuel = sum . takeWhile (>0) . tail . iterate f 
