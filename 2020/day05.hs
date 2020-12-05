a7a238742e1ed1bf2dd8b6b52d925d40cc4dd30a
Juri Dispan <juri.dispan@uni-ulm.de>
2020-12-05 13:47:26 +0100

    day 5

---
+++ 2020/day05.hs
@@ -0,0 +1,26 @@
+import           Data.List
+import           Data.Bifunctor
+
+seatId :: String -> Int
+seatId =
+  uncurry (+) . first (* 8) . bimap decodeBinary decodeBinary . splitAt 7
+
+decodeBinary :: String -> Int
+decodeBinary str = go str 0
+ where
+  go :: String -> Int -> Int
+  go []       n = n
+  go (x : xs) n = go xs (2 * n + (if x `elem` "FL" then 0 else 1))
+
+findMissing :: [Int] -> Int
+findMissing (x : xs) = go x xs
+ where
+  go n (n' : ns) | n + 1 /= n' = n + 1
+                 | otherwise   = go n' ns
+
+main :: IO ()
+main = do
+  inp <- lines <$> getContents
+  let seatNrs = map seatId inp
+  print $ maximum seatNrs
+  print $ findMissing $ sort seatNrs
