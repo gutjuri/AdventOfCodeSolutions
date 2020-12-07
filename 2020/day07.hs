b732fa04f5a558b4c1be0e02058f096899473111
Juri Dispan <juri.dispan@uni-ulm.de>
2020-12-07 12:13:28 +0100

    day 7

---
+++ 2020/day07.hs
@@ -0,0 +1,34 @@
+import qualified Data.Map.Strict               as M
+import           Data.Map.Strict                ( Map )
+
+parseLine :: String -> Map String (Map String Int)
+parseLine str = M.singleton parentCol ruleS
+ where
+  tokens    = words str
+  parentCol = unwords $ take 2 tokens
+  ruleS     = go $ drop 4 tokens
+  go ("no"            : _ ) = M.empty
+  go (n : c1 : c2 : _ : xs) = M.insert (c1 <> " " <> c2) (read n) (go xs)
+  go []                     = M.empty
+
+buildRuleset :: [String] -> Map String (Map String Int)
+buildRuleset = M.unions . map parseLine
+
+canReachShinyGold :: Map String (Map String Int) -> String -> Bool
+canReachShinyGold rls n = M.member "shiny gold" rule
+  || any (canReachShinyGold rls) (M.keys rule)
+  where rule = M.findWithDefault M.empty n rls
+
+mustContain :: Map String (Map String Int) -> String -> Int
+mustContain rls n = M.foldrWithKey'
+  (\childCol childN acc -> childN * (1 + mustContain rls childCol) + acc)
+  0
+  rule
+  where rule = M.findWithDefault M.empty n rls
+
+main :: IO ()
+main = do
+  inp <- lines <$> getContents
+  let rules = buildRuleset inp
+  print $ length $ filter (canReachShinyGold rules) (M.keys rules)
+  print $ mustContain rules "shiny gold"
